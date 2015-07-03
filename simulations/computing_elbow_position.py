46c38b647394d87a5925ef4c4ed271d19d29db18
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-07-03 16:01:36 +0100

    Computes elbow position in world space -- UNTESTED YET

--- simulations/computing_elbow_position.py
+++ simulations/computing_elbow_position.py
@@ -1,5 +1,15 @@
 __author__ = 'Petrut Bogdan'
 
+"""
+Recipe to compute the position of the elbow:
+- Compute the necessary rotation matrix for the upper arm
+- Have handy the position of the elbow in relation to the shoulder
+- Compute this product
+- Add position of the shoulder in relation to the world space
+- ??????
+- PROFIT
+"""
+
 import nengo
 import numpy as np
 from nengo.dists import Choice
@@ -10,6 +20,7 @@ h = 0.2
 upper_length = 0.3
 lower_length = 0.35
 
+#
 radius = l + h + upper_length + lower_length
 
 alpha = 1.
@@ -19,15 +30,15 @@ beta = 1.
 
 model = nengo.Network("Computing shoulder position")
 
-position_mat = np.asarray([[0], [0], [0]])
+position_mat = np.asarray([0, 0, 0])
 
-shoulder_position_mat = np.asarray([[l], [0], [-h]])
+shoulder_position_mat = np.asarray([l, 0, -h])
 
 rotation_mat = np.asarray(np.eye(3))
 
-elbow_position_mat = np.asarray([0, upper_length, 0])
+elbow_position_mat = np.asarray([[0, upper_length, 0]])
 
-bottom_row = [0, 0, 0, 1]
+# bottom_row = [0, 0, 0, 1]
 
 N = 50
 
@@ -61,11 +72,13 @@ with model:
                                          -np.sin(x), 0, np.cos(x)])
 
     combo = nengo.networks.EnsembleArray(N,
-                                     n_ensembles=rotation_mat.size * rotation_mat.shape[1],
-                                     ens_dimensions=2,
-                                     radius=1.5 * radius,
-                                     encoders=Choice(
-                                         [[1, 1], [-1, 1], [1, -1], [-1, -1]]))
+                                         n_ensembles=rotation_mat.size *
+                                                     rotation_mat.shape[1],
+                                         ens_dimensions=2,
+                                         radius=1.5 * radius,
+                                         encoders=Choice(
+                                             [[1, 1], [-1, 1], [1, -1],
+                                              [-1, -1]]))
 
 transformA = np.zeros((combo.dimensions, rotation_mat.size))
 transformB = np.zeros((combo.dimensions, rotation_mat.size))
@@ -82,12 +95,12 @@ with model:
     nengo.Connection(Ry.output, combo.input, transform=transformA)
     nengo.Connection(Rx.output, combo.input, transform=transformB)
 
-
 with model:
     # Now compute the products and do the appropriate summing
-    D = nengo.networks.EnsembleArray(N,
-                                     n_ensembles=rotation_mat.shape[0] * rotation_mat.shape[1],
-                                     radius=radius)
+    D = nengo.networks.EnsembleArray(
+        N, n_ensembles=rotation_mat.shape[0] * rotation_mat.shape[1],
+        radius=radius)
+
 
 def product(x):
     return x[0] * x[1]
@@ -104,3 +117,47 @@ with model:
     prod = combo.add_output("product", product)
     nengo.Connection(prod, D.input, transform=transformC)
 
+# Compute the position of the elbow in relation to the shoulder
+# This involves multiplying the position vector of the elbow by the rotation
+# matrix of the shoulder frame
+
+with model:
+    elbow_combo = nengo.networks.EnsembleArray(
+        N, n_ensembles=elbow_position_mat.size * rotation_mat.shape[1],
+        ens_dimensions=2,
+        radius=1.5 * radius,
+        encoders=Choice([[1, 1], [-1, 1], [1, -1], [-1, -1]]))
+
+transformElbow = np.zeros((elbow_combo.dimensions, elbow_position_mat.size))
+transformRotation = np.zeros((elbow_combo.dimensions, rotation_mat.size))
+
+for i in range(elbow_position_mat.shape[0]):
+    for j in range(elbow_position_mat.shape[1]):
+        for k in range(rotation_mat.shape[1]):
+            tmp = (j + k * elbow_position_mat.shape[1] + i * rotation_mat.size)
+            transformElbow[tmp * 2][j + i * elbow_position_mat.shape[1]] = 1
+            transformRotation[tmp * 2 + 1][k + j * rotation_mat.shape[1]] = 1
+
+with model:
+    nengo.Connection(E.output, elbow_combo.input, transform=transformElbow)
+    nengo.Connection(D.output, elbow_combo.input, transform=transformRotation)
+    elbow_in_world_space = nengo.networks.EnsembleArray(
+        N, n_ensembles=elbow_position_mat.shape[0] * rotation_mat.shape[1],
+        radius=radius)
+
+transformElbow2 = np.zeros((elbow_in_world_space.dimensions, rotation_mat.size))
+for i in range(rotation_mat.size):
+    transformElbow2[i // rotation_mat.shape[0]][i] = 1
+
+with model: # TODO Check that this outputs correct values
+    prod2 = elbow_combo.add_output("product", product)
+    nengo.Connection(prod2, elbow_in_world_space.input, transform=transformElbow2)
+
+
+# World space position computation
+
+with model:
+    adder = nengo.networks.EnsembleArray(N, n_ensembles=3, radius=2 * radius)
+
+    nengo.Connection(elbow_in_world_space.output, adder.input)
+    nengo.Connection(S.output, adder.input)
