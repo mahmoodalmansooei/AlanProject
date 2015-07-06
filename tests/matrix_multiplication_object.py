f8604a604fb1432b8aaa021d12e4ea3e5641dc93
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-07-06 14:05:50 +0100

    Preliminary test of a functioning matrix multiplier object

---
+++ tests/matrix_multiplication_object.py
@@ -0,0 +1,18 @@
+__author__ = 'Petrut Bogdan'
+
+import nengo
+from robot_models.matrix_multiplication import MatrixMultiplication
+import numpy as np
+
+model = nengo.Network("MM Object Test")
+A = np.asarray([[1]])
+B = np.asarray([[1, 2, 3]])
+with model:
+    mm = MatrixMultiplication(50, A, B)
+
+    inputA = nengo.Node(A.ravel())
+    inputB = nengo.Node(B.ravel())
+
+    nengo.Connection(inputA, mm.input_A)
+    nengo.Connection(inputB, mm.input_B)
+
