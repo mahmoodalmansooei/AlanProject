3e9dfba48c684b58d90658c777c70ba59c8bd15e
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-08-07 14:43:15 +0100

    Updated action test to include 3 actions, 2 lip enables, 2 mirror hand positions

--- tests/action_test.py
+++ tests/action_test.py
@@ -6,17 +6,20 @@ robot_control.action = reload(robot_control.action)
 
 model = nengo.Network(label="NetworkName")
 
-testing_lip_position = np.asarray(np.linspace(1, 0, 3))
-testing_hand_position = np.asarray(np.linspace(0, -1, 3))
+testing_lip_position = np.asarray([.2, .2, 0])
+testing_left_hand_position = np.asarray([-.3, .5, -.7])
+testing_right_hand_position = np.asarray([.3, .5, -.7])
 with model:
-    actions = nengo.Node(output=lambda t: [1, 0])
-    lip_enable = nengo.Node(output=lambda t: [0])
+    actions = nengo.Node(output=lambda t: [1, 0, 1])
+    lip_enable = nengo.Node(output=lambda t: [0, 0])
     lip_position = nengo.Node(testing_lip_position.ravel())
-    hand_position = nengo.Node(testing_hand_position.ravel())
+    left_hand_position = nengo.Node(testing_left_hand_position.ravel())
+    right_hand_position = nengo.Node(testing_right_hand_position.ravel())
 
-    selection = robot_control.action.ActionSelectionExecution(2)
+    selection = robot_control.action.ActionSelectionExecution(3)
 
     nengo.Connection(actions, selection.actions.input)
     nengo.Connection(lip_enable, selection.lip_enable)
     nengo.Connection(lip_position, selection.lip_position)
-    nengo.Connection(hand_position, selection.hand_position)
+    nengo.Connection(left_hand_position, selection.left_hand_position)
+    nengo.Connection(right_hand_position, selection.right_hand_position)
