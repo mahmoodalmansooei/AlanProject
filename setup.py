3c722d8fb09b729a0ff24f38a78df0b7f89584c9
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-08-10 14:35:06 +0100

    setup py contains requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='AlanRobot',
+    name='alan_robot',
     version='1.0.0',
     packages=find_packages(),
     # package_dir={'robot_control': ''},
@@ -9,5 +9,8 @@ setup(
     license='',
     author='Petrut Antoniu Bogdan',
     author_email='petrut.bogdan@student.manchester.ac.uk',
-    description='Spiking neural network robot control system running on SpiNNaker'
+    description='Spiking neural network robot control system running on SpiNNaker',
+    # Requirements
+    install_requires=["nengo>=2.0.0, <3.0.0", "rig>=0.5.3, <1.0.0",
+                      "bitarray>=0.8.1, <1.0.0", "nengo_spinnaker>=0.2.4"],
 )
