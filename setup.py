41eac31d7234e12fad9522b86a2a7e5699b0ecf9
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-08-10 12:03:27 +0100

    setup py should recognise packages now

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='AlanRobot',
     version='1.0.0',
-    packages=[''],
-    package_dir={'': 'robot_control'},
-    url='',
+    packages=find_packages(),
+    # package_dir={'robot_control': ''},
+    url='http://pabogdan.github.io/AlanProject',
     license='',
     author='Petrut Antoniu Bogdan',
     author_email='petrut.bogdan@student.manchester.ac.uk',
