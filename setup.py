b3557a403d1804cf6448c22ff8504c8a2b1a0504
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-08-10 11:47:20 +0100

    First setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='AlanRobot',
+    version='1.0.0',
+    packages=[''],
+    package_dir={'': 'robot_control'},
+    url='',
+    license='',
+    author='Petrut Antoniu Bogdan',
+    author_email='petrut.bogdan@student.manchester.ac.uk',
+    description='Spiking neural network robot control system running on SpiNNaker'
+)
