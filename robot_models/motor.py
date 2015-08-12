5fbd9d7acf711a5af92e9c00158f5a9a54cb6263
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-08-12 16:03:35 +0100

    Implemented a Motor node. Compatible with Container from robot_interface

--- robot_models/motor.py
+++ robot_models/motor.py
@@ -4,13 +4,41 @@ import nengo
 
 
 class Motor(nengo.Node):
-    def __init__(self, sampling_period=100, dt=1000., label=None):
+    def __init__(self, container, sampling_period=100, dt=1000., label=None):
+        """
+        A motor is a type of node that sends live information back for
+        processing. This is different from a standard node because it only
+        sends information at a certain frequency, not at every change due to
+        limitations in the communication channel.
+        :param container: The object that receives information from the motor
+        :type container: Container or ContainerThread
+        :param sampling_period: The period with which the motor sends information
+        :type sampling_period: float or int
+        :param dt: The time step
+        :type dt: float
+        :param label: The name of the motor
+        :type label: str
+        :return: A motor node
+        :rtype: Motor
+        """
         self.sampling_period = sampling_period / dt  # ms
         self.previous_time = - self.sampling_period
+        self.container = container
         super(Motor, self).__init__(output=self.motor_output, size_in=1,
                                     label=label)
 
-    def motor_output(self, t, x):
-        if t - self.previous_time >= self.sampling_period:
-            self.previous_time = t
-            print self.label, t, x
+    def motor_output(self, time, values):
+        """
+        Function that is called every time tick for outputting values from the
+        node.
+        :param time: The current simulation time
+        :type time: float
+        :param values: The current values of the motor node (number of values
+        depends on the dimensionality of the node)
+        :type values: array of floats
+        :return: None
+        :rtype:
+        """
+        if time - self.previous_time >= self.sampling_period:
+            self.previous_time = time
+            self.container.update(self, (time, values))
