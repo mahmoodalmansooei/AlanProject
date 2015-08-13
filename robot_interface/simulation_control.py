97d758eef971e6f08dfa28c16397abfe0d06a7e2
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-08-13 16:25:11 +0100

    Implemented the simulation control which allows the user to start or stop the simulation

---
+++ robot_interface/simulation_control.py
@@ -0,0 +1,35 @@
+__author__ = 'Petrut Bogdan'
+
+import logging
+import threading
+
+logger = logging.getLogger(__name__)
+
+
+class SimulationControl(threading.Thread):
+    def __init__(self, simulator, run_time=None):
+        """
+        A class that controls the simulation. It provides start and stop control
+        over the simulation.
+        :param simulator: A neural simulator (e.g. nengo Simulator or
+        nengo_spinnaker Simulator)
+        :type simulator: Simulator
+        :param run_time: Time in seconds
+        :type run_time: int
+        :return:
+        :rtype:
+        """
+        super(SimulationControl, self).__init__(name="SimulationControl Thread")
+        self.simulator = simulator
+        self.run_time = run_time
+
+    def run(self):
+        logger.log(logging.DEBUG, "Running the simulation in the " + self.name)
+        with self.simulator:
+            self.simulator.run(self.run_time)
+
+    def stop(self):
+        """Stop a continuously running simulation or cut short a fixed
+        length simulation"""
+        logger.log(logging.DEBUG, "Stopping the " + self.name)
+        self.simulator.stop()
