88ba68680ee202565af064054cee06055c8ec5ee
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-08-14 10:44:11 +0100

    Sensor nodes seem to (updating them using  a container)

---
+++ robot_models/sensor.py
@@ -0,0 +1,45 @@
+__author__ = 'Petrut Bogdan'
+
+import nengo
+
+
+class Sensor(nengo.Node):
+    def __init__(self, container, sampling_period=100, dt=0.001, label=None):
+        """
+        A motor is a type of node that sends live information back for
+        processing. This is different from a standard node because it only
+        sends information at a certain frequency, not at every change due to
+        limitations in the communication channel.
+        :param container: The object that receives information from the motor
+        :type container: Container or ContainerThread
+        :param sampling_period: The period with which the motor sends information
+        :type sampling_period: float or int
+        :param dt: The time step in seconds
+        :type dt: float
+        :param label: The name of the motor
+        :type label: str
+        :return: A motor node
+        :rtype: Motor
+        """
+        self.sampling_period = sampling_period * dt  # ms
+        self.previous_time = - self.sampling_period
+        self.container = container
+        super(Sensor, self).__init__(output=self.sensor_output, size_out=1,
+                                     label=label)
+
+    def sensor_output(self, time):
+        """
+        Function that is called every time tick for outputting values from the
+        node.
+        :param time: The current simulation time
+        :type time: float
+        :param values: The current values of the motor node (number of values
+        depends on the dimensionality of the node)
+        :type values: array of floats
+        :return: None
+        :rtype:
+        """
+        # if time - self.previous_time >= self.sampling_period:
+        #     self.previous_time = time
+        # TODO Make transmit period be adjustable
+        return self.container[self]
