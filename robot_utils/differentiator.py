5e7b29cbdb59f97d2896f4c7c9ce31e6173a4b6b
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2015-08-20 12:17:04 +0100

    Differentiator docs completed

--- robot_utils/differentiator.py
+++ robot_utils/differentiator.py
@@ -5,11 +5,38 @@ class Differentiator(nengo.Network):
     def __init__(self, n_neurons,
                  radius=1.0, tau=0.1,
                  label=None, seed=None,
-                 add_to_container=None, **ens_kwargs):
+                 add_to_container=None):
+        """
+        This network takes as input a signal (function of time) and outputs
+        the differentiated result (the slope of the signal).
+
+        The opposite of an :py:func:`~nengo.networks.Integrator`.
+
+        Applying :py:func:`~nengo.networks.Integrator` on the output of the
+        Differentiator will result in an approximation of the original signal.
+
+        :param n_neurons: The number of neurons.
+        :type n_neurons: int
+        :param radius: The range of values that can be represented
+        :type radius: float
+        :param tau: post synaptic time constant
+        :type tau: float
+        :param label:  str, optional
+        :type label: Name of the model. Defaults to None.
+        :param seed: int, optional
+        :type seed: Random number seed that will be fed to the random
+            number generator. Setting this seed makes the creation of the
+            model a deterministic process; however, each new ensemble
+            in the network advances the random number generator, so if
+            the network creation code changes, the entire model changes.
+        :param add_to_container: bool, optional
+        :type add_to_container: Determines if this Network will be added
+            to the current container. Defaults to true iff
+            currently with a Network.
+        """
         super(Differentiator, self).__init__(label, seed,
                                              add_to_container)
         # region Variable assignment
-        self.config[nengo.Ensemble].update(ens_kwargs)
         self.n_neurons = n_neurons
         self.radius = radius
         self.tau = tau
