f832996da8c1349a8cf8c7fa4e5428c348a60f1b
bogdanp <petrutantoniu8@gmail.com>
2015-08-21 11:43:24 +0100

    modified setup.py to use stable versions of nengo and nengo_spinnaker

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
     author_email='petrut.bogdan@student.manchester.ac.uk',
     description='Spiking neural network robot control system running on SpiNNaker',
     # Requirements
-    dependency_links = ['https://github.com/nengo/nengo/tarball/23107fe#egg=nengo-2.0.0',
-                        'https://github.com/pabogdan/nengo_spinnaker/tarball/master#=nengo_spinnaker-0.2.4'],
+    dependency_links = ['https://github.com/nengo/nengo/tarball/23107fe#egg=nengo-2.1.0.dev0',
+                        'https://github.com/pabogdan/nengo_spinnaker/tarball/master#egg=nengo-spinnaker-0.2.4'],
 
-    install_requires=["nengo>=2.0.0, <3.0.0", "rig>=0.5.3, <1.0.0",
-                      "bitarray>=0.8.1, <1.0.0", "nengo_spinnaker>=0.2.4"],
+    install_requires=["nengo==2.1.0.dev0", "rig>=0.5.3, <1.0.0",
+                      "bitarray>=0.8.1, <1.0.0", "nengo-spinnaker==0.2.4"],
     classifiers=[
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
