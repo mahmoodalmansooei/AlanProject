10b4fbe39527d8db80a306c740fe2c10fef2fdc9
pabogdan <dwTheresnosugar2013>
2015-11-17 13:57:41 +0000

    Proof of concept for a SPA based robot control system

---
+++ tests/spa_state_transitions.py
@@ -0,0 +1,52 @@
+from nengo.processes import WhiteNoise
+
+__author__ = 'bogdanp'
+
+import nengo
+import nengo.spa as spa
+import numpy as np
+
+dimensions = 64  # the dimensionality of the vectors
+
+
+model = spa.SPA()
+with model:
+    model.cortex = spa.Buffer(dimensions=dimensions, neurons_per_dimension=100)
+    model.motor = spa.Buffer(dimensions=dimensions, neurons_per_dimension=100)
+    model.buffer = spa.Buffer(dimensions=dimensions, neurons_per_dimension=100)
+    # model.buffer2 = spa.Buffer(dimensions=dimensions, neurons_per_dimension=100)
+    model.direction = spa.Memory(dimensions=dimensions, tau=0.01)
+    # model.time = nengo.networks.Oscillator(1, 1, 500)
+    # model.perceived_time = spa.Memory(dimensions=dimensions, tau=0.1)
+    # Specify the action mapping
+    actions = spa.Actions(
+        '.8 * dot(cortex, S) + dot(buffer, L)--> motor = LS, cortex = S',
+        '.8 * dot(cortex, S) + dot(buffer, R)--> motor = RS, cortex = S',
+        '.8 * dot(cortex, G) + dot(buffer, L)--> motor = LG, cortex = G',
+        '.8 * dot(cortex, G) + dot(buffer, R)--> motor = RG, cortex = G',
+    )
+
+    cortical_actions = spa.Actions(
+        'buffer= buffer + .1 * direction '
+    )
+    model.cortical = spa.Cortical(cortical_actions)
+
+    model.bg = spa.BasalGanglia(actions=actions)
+    model.thal = spa.Thalamus(model.bg)
+
+
+def start(t):
+    if t < 0.1:
+        return 'G'
+    else:
+        return '0'
+
+def update_direction(_):
+    if np.random.normal(0, 1, 1) > 0:
+        return 'L'
+    else:
+        return 'R'
+
+with model:
+    model.input = spa.Input(cortex=start)
+    model.rand_input = spa.Input(direction=update_direction)
